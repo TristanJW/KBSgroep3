2cf63505a1af0765a0d52412dd22d740588e368a
jaschafix <jascha.fix555@gmail.com>
2019-05-10 13:37:35 +0200

    Voorbeeld voor tristan
    Voorbeeld gemaakt van leveranciers class

--- src/Applicatie/Main.java
+++ src/Applicatie/Main.java
@@ -1,24 +1,32 @@
 package Applicatie;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
         Scherm scherm1 = new Scherm();
         
-//        JDBC database = new JDBC();
-//
-//        ResultSet resultaat = database.dataOphalen("SELECT * From leverancierslijst");
-//        try {
-//            while (resultaat.next()) {
-//                int id = resultaat.getInt("Itemid");
-//                String naam = resultaat.getString("naam");
-//
-//                System.out.println("ID:" + id + " naam:" + naam);
-//            }
-//        } catch (SQLException se) {
-//            se.printStackTrace();
-//        }
+        JDBC database = new JDBC();
+
+        ResultSet resultaat = database.dataOphalen("SELECT * From leverancierslijst");
+        try {
+            while (resultaat.next()) {
+                int id = resultaat.getInt("Itemid");
+                String naam = resultaat.getString("naam");
+
+                System.out.println("ID:" + id + " naam:" + naam);
+            }
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+        
+        LeveranciersLijst leverancier = new LeveranciersLijst();
+        ArrayList<NetwerkComponent> servers = leverancier.aanbodWebserver;
+        
+        for(NetwerkComponent s : servers){
+            System.out.println(s.getNaam());
+        }
     }
 }
