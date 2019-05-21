2a5f13a8ad4039a763ab710d39a98d9c5cd57860
jaschafix <jascha.fix555@gmail.com>
2019-05-21 13:22:29 +0200

    Update Main.java
    betere main

--- src/Applicatie/Main.java
+++ src/Applicatie/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        //  Scherm scherm1 = new Scherm();
+          Scherm scherm1 = new Scherm();
 //
 //        JDBC database = new JDBC();
 //
@@ -29,30 +29,5 @@ public class Main {
 //            System.out.println(s.getNaam());
 //        }
 
-        HuidigeConfiguratie netwerk = new HuidigeConfiguratie();
-        Algoritme algoritme = new Algoritme();
-        LeveranciersLijst leverancier = new LeveranciersLijst();
-//        netwerk.voegToe(leverancier.aanbodFirewall.get(0));
-//        netwerk.voegToe(leverancier.aanbodLoadBalancer.get(0));
-//        netwerk.voegToe(leverancier.aanbodWebserver.get(0));
-//        netwerk.voegToe(leverancier.aanbodDBServer.get(1));
-//        
-//        for(int i =0; i < 4; i++){
-//            netwerk.voegToe(leverancier.aanbodWebserver.get(1));
-//        }
-//        
-//        for(int i =0; i < 3; i++){
-//            netwerk.voegToe(leverancier.aanbodDBServer.get(0));
-//        }
-//        
-//        for(NetwerkComponent nc: netwerk.getNetwerkLijst()){
-//            System.out.print(nc.getNaam() + " ");
-//        }
-//        
-//        System.out.println(netwerk.berekenBeschikbaarheid() + " " + netwerk.berekenTotalePrijs());
-//        System.out.println("webservers: " + netwerk.berekenComponent(Webserver.class));
-//        System.out.println("DBServers: " + netwerk.berekenComponent(DBServer.class));
-        
-        algoritme.maakCombinatie(99.99);
     }
 }
